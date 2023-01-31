46560b2381f420cf186d1868362604f2fac68552
maheshsabnis <sabnis_m@hotmail.com>
2023-01-31 16:50:55 +0530

    Add project files.

---
+++ ApacheKafkaConsumerDemo/BackgroundServices/ApacheKafkaConsumerService.cs
@@ -0,0 +1,63 @@
+﻿using ApacheKafkaConsumerDemo.Models;
+using Confluent.Kafka;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace ApacheKafkaConsumerDemo.BackgroundServices
+{
+    public class ApacheKafkaConsumerService : IHostedService
+    {
+        private readonly string topic = "test";
+        private readonly string groupId = "test_group";
+        private readonly string bootstrapServers = "localhost:9092";
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = groupId,
+                BootstrapServers = bootstrapServers,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            try
+            {
+                using (var consumerBuilder = new ConsumerBuilder
+                <Ignore, string>(config).Build())
+                {
+                    consumerBuilder.Subscribe(topic);
+                    var cancelToken = new CancellationTokenSource();
+
+                    try
+                    {
+                        while (true)
+                        {
+                            // COnsume the MEssage from Topic
+                            var consumer = consumerBuilder.Consume
+                               (cancelToken.Token);
+                            var orderRequest = JsonSerializer.Deserialize<OrderProcessingRequest>
+                                    (consumer.Message.Value);
+                            Debug.WriteLine($"Processing Order Id: { orderRequest.OrderId}");
+
+                            Debug.WriteLine($"Processing Order Id: {JsonSerializer.Serialize<OrderProcessingRequest> (orderRequest)}");
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumerBuilder.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ApacheKafkaProducerDemo/Models/OrderRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace ApacheKafkaProducerDemo.Models
+{
+    namespace ApacheKafkaProducerDemo
+    {
+        public class OrderRequest
+        {
+            public int OrderId { get; set; }
+            public int ProductId { get; set; }
+            public int CustomerId { get; set; }
+            public int Quantity { get; set; }
+            public string Status { get; set; }
+        }
+    }
+
+}
